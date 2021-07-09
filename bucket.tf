1fff7d327dcdaf5b28cb10fc6c50b0c5943f0729
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-07-09 09:23:17 +0200

    Add: AWS077 - S3 Data should be versioned
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- bucket.tf
+++ bucket.tf
@@ -16,7 +16,12 @@ resource "aws_s3_bucket" "velero_log" {
   bucket        = format("%s-log", local.service_name)
   acl           = "log-delivery-write"
   force_destroy = true
-  tags          = var.tags
+
+  tags = var.tags
+
+  versioning {
+    enabled = true
+  }
 
   server_side_encryption_configuration {
     rule {
@@ -32,7 +37,12 @@ resource "aws_s3_bucket" "velero" {
   bucket        = local.service_name
   acl           = "private"
   force_destroy = true
-  tags          = var.tags
+
+  tags = var.tags
+
+  versioning {
+    enabled = true
+  }
 
   logging {
     target_bucket = aws_s3_bucket.velero_log.id
